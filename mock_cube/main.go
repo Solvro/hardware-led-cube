82f2000ce9111caa53808dd68846d3320b4d329b
lmProgramming <mikikubs@gmail.com>
2025-02-12 17:28:41 +0100

    feat: there is a cube and it's spinning

--- mock_cube/main.go
+++ mock_cube/main.go
@@ -1,39 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+// type Cube struct {
+// 	ID int
+// }
 
-type Cube struct {
-	ID int
-}
+// func (c *Cube) Render() {
+// 	fmt.Printf("Renderuję kostkę o ID: %d\n", c.ID)
+// }
 
-func (c *Cube) Render() {
-	fmt.Printf("Renderuję kostkę o ID: %d\n", c.ID)
-}
+// func main() {
+// 	renderChan := make(chan *Cube)
 
-func main() {
-	renderChan := make(chan *Cube)
+// 	go func() {
+// 		for {
+// 			cube := <-renderChan
 
-	go func() {
-		for {
-			cube := <-renderChan
+// 			if cube != nil {
+// 				cube.Render()
+// 			}
+// 		}
+// 	}()
 
-			if cube != nil {
-				cube.Render()
-			}
-		}
-	}()
+// 	// temporary cube generation
+// 	id := 1
+// 	for {
+// 		cube := &Cube{ID: id}
+// 		id++
 
-	// temporary cube generation
-	id := 1
-	for {
-		cube := &Cube{ID: id}
-		id++
+// 		renderChan <- cube
 
-		renderChan <- cube
-
-		time.Sleep(16 * time.Millisecond)
-	}
-}
+// 		time.Sleep(16 * time.Millisecond)
+// 	}
+// }
