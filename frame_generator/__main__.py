468d3190b2eb0ba30a3a0aabbe94fce8a3ce2f15
jantar <mikikubs@gmail.com>
2025-02-27 17:23:43 +0100

    feat: added SimpleRotatingSolvro

--- frame_generator/__main__.py
+++ frame_generator/__main__.py
@@ -1,14 +1,14 @@
-from frame_generator.state_parser import PrototypeJsonifier
+from frame_generator.state_parser import *
 from frame_generator.animator import *
-from frame_generator.parsed_handler import Saver
+from frame_generator.parsed_handler import *
 from frame_generator.main import generate_frames
 
 ANIMATION_NAME = "animation"
 
 if __name__ == "__main__":
     width = 8
-    state_parser = PrototypeJsonifier()
-    sample_animator = TwoFacedSolvro(width)
+    state_parser = PrototypeJsonifierWithBuffer(30)
+    sample_animator = SimpleRotatingSolvro(width)
     saver = Saver(ANIMATION_NAME)
     generate_frames((width, width, width), state_parser,
                     sample_animator, saver)
--- frame_generator/animator.py
+++ frame_generator/animator.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from PIL.ImageFile import ImageFile
 import numpy.typing as npt
 from PIL import Image
+from frame_generator.image_utils import get_formatted_image_data
 
 
 class Animator(ABC):
@@ -47,20 +48,13 @@ class RGBMovingRight(Animator):
         return frames == leds.shape[0] - 1
 
 
-class TwoFacedSolvro(Animator):
+class TwoPlaneSolvro(Animator):
     def internal_setup(self, cube_width: int) -> None:
         img: ImageFile = Image.open('frame_generator/solvro8x8x8.png')
 
-        self.data: list[list[float]
-                        ] = TwoFacedSolvro.get_formatted_image_data(img)
+        self.data: list[list[float]] = get_formatted_image_data(img)
         self.counter = 0
-
-    @staticmethod
-    def get_formatted_image_data(img) -> list[list[float]]:
-        data = list(img.getdata())
-        data.reverse()
-        data = [[c/255 for c in color[:3]] for color in data]
-        return data
+        self.plane_index: int = cube_width // 2
 
     def start_function(self, leds):
         return self.update_function(leds)
@@ -69,19 +63,71 @@ class TwoFacedSolvro(Animator):
         self.counter += 1
         is_even_frame = self.counter % 2 == 0
 
-        plane_index = leds.shape[0] // 2
+        for x in range(leds.shape[0]):
+            for y in range(leds.shape[1]):
+                for z in range(leds.shape[2]):
+                    leds[x][y][z] = [0, 0, 0]
+
+                    if is_even_frame and z == self.plane_index:
+                        leds[x][y][z] = self.data[x + y * leds.shape[0]]
+                    elif not is_even_frame and x == self.plane_index:
+                        leds[x][y][z] = self.data[z + y * leds.shape[0]]
+
+        return leds
+
+    def stop_function(self, leds, frames):
+        return self.counter > 2
+
+
+class SimpleRotatingSolvro(Animator):
+    def internal_setup(self, cube_width: int) -> None:
+        img: ImageFile = Image.open('frame_generator/solvro8x8x8.png')
+        self.data: list[list[float]] = get_formatted_image_data(img)
+        self.counter = 0
+        self.middle = cube_width // 2
+
+    def start_function(self, leds):
+        return self.update_function(leds)
+
+    def update_function(self, leds):
+        self.counter += 1
+        mode = self.counter % 4
 
         for x in range(leds.shape[0]):
             for y in range(leds.shape[1]):
                 for z in range(leds.shape[2]):
                     leds[x][y][z] = [0, 0, 0]
 
-                    if is_even_frame and z == plane_index:
-                        leds[x][y][z] = self.data[x + y * leds.shape[0]]
-                    elif not is_even_frame and x == plane_index:
-                        leds[x][y][z] = self.data[z + y * leds.shape[0]]
+        width = leds.shape[0]
+
+        if mode == 0:
+            for x in range(width):
+                for y in range(width):
+                    index = x + y * width
+                    if index < len(self.data):
+                        leds[x][y][self.middle] = self.data[index]
+        elif mode == 1:
+            for z in range(width):
+                for y in range(width):
+                    mapped_x = width - 1 - z
+                    index = mapped_x + y * width
+                    if index < len(self.data):
+                        leds[self.middle][y][z] = self.data[index]
+        elif mode == 2:
+            for x in range(width):
+                for y in range(width):
+                    mapped_x = width - 1 - x
+                    index = mapped_x + y * width
+                    if index < len(self.data):
+                        leds[x][y][self.middle] = self.data[index]
+        elif mode == 3:
+            for z in range(width):
+                for y in range(width):
+                    index = z + y * width
+                    if index < len(self.data):
+                        leds[self.middle][y][z] = self.data[index]
 
         return leds
 
     def stop_function(self, leds, frames):
-        return self.counter > 0
+        return self.counter > 16
