4258e20e3e0f362300aa81cb4688fab97ae700e7
kaykoe <karolkoenigg@gmail.com>
2025-03-04 13:51:17 +0100

    fix: remove redundant go installation and modified install directories to be idiomatic in linux

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -3,11 +3,6 @@
 # Install scons
 apt-get install scons
 
-# Install Go
-wget https://golang.org/dl/go1.20.3.linux-armv6l.tar.gz
-tar -C /usr/local -xzf go1.20.3.linux-armv6l.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-
 # Install rpi_ws281x
 cd /home/dietpi/
 wget https://github.com/jgarff/rpi_ws281x/archive/refs/tags/v1.0.0.tar.gz
@@ -19,8 +14,8 @@ cp *.h /usr/local/include
 
 # Clone and build the hardware-led-cube project
 cd ..
-git clone --branch main --single-branch https://github.com/Solvro/hardware-led-cube.git /home/src
-cd /home/src
+git clone --branch main --single-branch https://github.com/Solvro/hardware-led-cube.git ~/src
+cd ~/src
 go build
 
 # Create systemd service file
@@ -30,8 +25,8 @@ Description=Hardware LED Cube Service
 After=network.target
 
 [Service]
-ExecStart=/home/src/hardware-led-cube
-WorkingDirectory=/home/src
+ExecStart=~/src/hardware-led-cube
+WorkingDirectory=~/src
 StandardOutput=inherit
 StandardError=inherit
 Restart=always
